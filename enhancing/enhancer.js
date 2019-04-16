b83ba34c910225f3bf9a848ceef625c88d556a02
Will Schulz <will.schulz29@gmail.com>
2019-04-16 11:14:44 -0400

    mvp complete

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,35 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  if (item.enhancement === 20) {
+    return { ...item };
+  } else {
+    const enhancementPlus = item.enhancement + 1;
+    return { ...item, enhancement: enhancementPlus };
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if(item.enhancement < 15){
+    const durabilityFail5 = item.durability - 5;
+    return { ...item, durability: durabilityFail5};
+  } else if(item.enhancement == 15){
+    const durabilityFail10 = item.durability - 10;
+    return { ...item, durability: durabilityFail10};
+  } else if(item.enhancement >= 16){
+    const highEnhancementFail = item.enhancement -1;
+    const highEnhancementDurabilityFail = item.durability - 10;
+    return { ...item, durability: highEnhancementDurabilityFail, enhancement: highEnhancementFail}
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  if(item.durability < 100) {
+    const durabilityRepair = item.durability = 100;
+    return { ...item, durability: durabilityRepair};
+  } else {
+    return { ...item }
+  }
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,33 @@
-const enhancer = require('./enhancer.js');
+const {repair, fail, succeed, get} = require('./enhancer.js');
+
 // test away!
+it('should always set damage back to 100', () => {
+    expect(repair({name: 'a', durability: 75, enhancement: 0})).toEqual(
+      {
+        name: 'a',
+        durability: 100,
+        enhancement: 0
+      }
+    );
+    
+});
+
+it('should always increase enhancement by 1 if between 0 and 15 ', () => {
+    expect(succeed({ name: 'a', durability: 10, enhancement: 19 })).toEqual(
+      {
+        name: 'a',
+        durability: 10,
+        enhancement: 20
+      }
+    )
+})
+
+it('should decrease durability by 5 if enhancement is <15, or by 10 if enhancement is >15, and decrease enhancement by 1 if >16', () => {
+  expect(fail({name: 'a', durability: 100, enhancement: 5})).toEqual(
+    {
+      name: 'a',
+      durability: 95,
+      enhancement: 5
+    }
+  )
+})
